c5dd718846a891e9a53fa45f80d879746b875ca6
Jelle <jmanzanohaagsma@gmail.com>
2022-10-13 17:52:52 +0200

    finished between Javier and myself using the remote control of zoom

--- index.js
+++ index.js
@@ -12,12 +12,38 @@ mongoose
   .connect(MONGODB_URI)
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    //? Iteración 2
+    /*return Recipe.create({
+      title: "Tortilla de Patata",
+      level:"Easy Peasy",
+      ingredients : ["huevo","cebolla","patata", "aceite", "sal"],
+      cuisine: "spanish",
+      dishType: "main_course",
+      image : "https://static2.abc.es/media/bienestar/2020/05/22/tortilla-patatas-k9tF--1200x630@abc.jpeg",
+      duration: 20,
+      creator: "la abuela"
+    })*/
+    //return Recipe.deleteMany()
+    //?Iteración 3
+    //return Recipe.insertMany(data)
+    //?Iteración 4
+    //return Recipe.findByIdAndUpdate("63483149a0f26987b7f2d747", {duration: 100}, {new:true})
+    //?Iteración 5
+    //return Recipe.deleteOne({title:"Carrot Cake"})
+    //? Iteración 6
+
   })
-  .then(() => {
+  .then((response) => {
+        //? CLG iteración 4: console.log("Updated Rigattoni", response)
+        console.log(response)
     // Run your code here, after you have insured that the connection was made
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+  mongoose.connection.close(function () {
+    console.log('Mongoose default connection closed');
+  });
+  
+
